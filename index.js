658311de66c6932e1e7fafeec7c9cab56e000b26
lonefy <lonefy@foxmail.com>
2016-04-11 18:40:32 +0800

    v0.0.2 add api

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var chalk = require('chalk');
 var _ = require('lodash');
 var mkdirp = require('mkdirp');
 var tpl = require('./template')();
+var minimatch = require('minimatch');
+
 
 var generator = module.exports
 
@@ -16,22 +18,50 @@ function findLastest(versions) {
     return t;
 }
 
+generator.getJSON = function(type) {
+    if(!hasType(type)) return;
+    return tpl[type].getJson();
+}
+
+generator.getType = function (){
+    var arr = [];
+    for(var i in tpl){
+        arr.push(i);
+    }
+    return arr;
+}
+
 generator.getTaskfilePath = function getTaskfilePath(type) {
+    if(!hasType(type)) return;
     return tpl[type].getTaskfilePath();
 }
 
-generator.template = function template(type, destination, checkexist) {
+generator.template = function template(type, destination, checkexist, glob) {
+    
+    if(!hasType(type)) return;
+      
     var t = tpl[type];
     var filepath = t.filePaths;
     var folderRoot = t.folderRoot;
-    var cwd = process.cwd();
-    
+  
     for (var i = 0, l = filepath.length; i < l; i++) {
+        if(glob !== undefined && !minimatch(filepath[i], glob)) continue;
+        
         var source = path.join(folderRoot, filepath[i]);
-        var destination = path.join(cwd, filepath[i])
-        output(source, destination, checkexist)
+        var filedest = path.join(destination, filepath[i])
+        output(source, filedest, checkexist)
     }
 
+}
+
+generator.templateMatch = function templateOne(type, destination, glob) {
+    this.template(type, destination , false, glob);
+}
+
+function hasType(type){
+    var flag = !!tpl[type];
+    !flag && console.log('No '+ type +' template');
+    return flag;
 }
 
 function output(source, destination, checkexist) {
